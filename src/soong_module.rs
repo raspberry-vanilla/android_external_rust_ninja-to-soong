735ba65aa70219dc5c80c9d5336221011f0ecec9
Konsta <konsta09@gmail.com>
2025-08-19 12:17:48 +0300

    Revert "Wildcardize later in the flow (#221)"
    * Doesn't seem to generate any wildcards when using --copy-to-aosp
      option.
    * With this reverted the wildcards are at least generated on consecutive
      runs (still not on the first clean run where Android.bp and
      meson_generated directory don't exist).
    
    This reverts commit 54955dee6dd807afecb92ea08a5c1bb8a79eabe3.

--- src/soong_module.rs
+++ src/soong_module.rs
@@ -35,7 +35,6 @@ pub enum SoongProp {
 pub struct SoongNamedProp {
     name: String,
     prop: SoongProp,
-    wildcard_src_path: Option<PathBuf>,
 }
 
 impl SoongNamedProp {
@@ -43,17 +42,7 @@ impl SoongNamedProp {
         Self {
             name: String::from(name),
             prop,
-            wildcard_src_path: None,
         }
-    }
-
-    pub fn enable_wildcard(&mut self, src_path: &Path) -> Result<(), String> {
-        match &self.prop {
-            SoongProp::VecStr(_) => (),
-            _ => return error!("Could not wildcardize a non-VecStr property"),
-        }
-        self.wildcard_src_path = Some(PathBuf::from(src_path));
-        Ok(())
     }
 
     pub fn get_prop(&self) -> SoongProp {
@@ -158,9 +147,6 @@ impl SoongNamedProp {
             SoongProp::VecStr(mut vec_str) => {
                 if vec_str.len() == 0 {
                     return String::new();
-                }
-                if let Some(src_path) = self.wildcard_src_path {
-                    vec_str = wildcardize_paths(vec_str, &src_path);
                 }
                 vec_str.sort_unstable();
                 vec_str.dedup();
--- src/soong_module_generator.rs
+++ src/soong_module_generator.rs
@@ -281,12 +281,11 @@ where
                 SoongProp::Str(path_to_string(strip_prefix(vs, &self.src_path))),
             );
         }
-        let mut srcs_prop = SoongNamedProp::new("srcs", SoongProp::VecStr(sources));
         if ctx.wildcardize_paths {
-            srcs_prop.enable_wildcard(&self.src_path)?;
+            sources = wildcardize_paths(sources, &self.src_path);
         }
         module = module
-            .add_named_prop(srcs_prop)
+            .add_prop("srcs", SoongProp::VecStr(sources))
             .add_prop("cflags", SoongProp::VecStr(cflags))
             .add_prop("ldflags", SoongProp::VecStr(link_flags))
             .add_prop("shared_libs", SoongProp::VecStr(shared_libs))
