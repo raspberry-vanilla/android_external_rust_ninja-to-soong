ca2f4a2aecaee8b28107d4023631c44916e4d9fd
Konsta <konsta09@gmail.com>
2026-01-24 11:44:03 +0200

    mesa3d-rpi: update to 25.3.4

--- tests/mesa3d-rpi/checkout.sh
+++ tests/mesa3d-rpi/checkout.sh
@@ -6,7 +6,7 @@ set -xe
 DEST="$1"
 SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 
-bash "${SCRIPT_DIR}/../../../utils/checkout.sh" https://github.com/raspberry-vanilla/android_external_mesa3d-rpi 566101e9e8c616b366dcdab242ee171476909797 "${DEST}/external/mesa3d-rpi"
+bash "${SCRIPT_DIR}/../../../utils/checkout.sh" https://github.com/raspberry-vanilla/android_external_mesa3d-rpi 4022f5437d4868b6db08729ebb957ee3dbeaa260 "${DEST}/external/mesa3d-rpi"
 
 sudo apt install \
     meson-1.5 \
